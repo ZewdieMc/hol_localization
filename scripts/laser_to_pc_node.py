d71412d30aa07e551bcb8708a940dbf2b6a7ba96
Zewdie <zewdiehabtie26@gmail.com>
2024-04-18 23:38:24 +0200

    Subscribe from stonefish rplidar

--- scripts/laser_to_pc_node.py
+++ scripts/laser_to_pc_node.py
@@ -18,16 +18,16 @@ def scan_cb(msg):
     pc2_msg = lp.projectLaser(msg) 
     # Convert ROS PointCloud2 message to NumPy array
     cloud_arr = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pc2_msg)
-    pc_pub.publish(pc2_msg)
+    pc_pub.publish(pc2_msg)#! octomap server subscribes to this topic
     # Create Open3D point cloud 
     pcd = oddd.geometry.PointCloud()
     pcd.points = oddd.utility.Vector3dVector(cloud_arr)
 
     # Save point cloud to PCD file
-    oddd.io.write_point_cloud("room_scan2.pcd", pcd, write_ascii=False, compressed=False, print_progress=True)
+    # oddd.io.write_point_cloud("room_scan2.pcd", pcd, write_ascii=False, compressed=False, print_progress=True)
     os.system("clear")
     rospy.loginfo("Point cloud saved\nPress Ctrl+C to stop contineouly scanning.")
 
-rospy.Subscriber("/scan", LaserScan, scan_cb, queue_size=1)
-rospy.Subscriber("/turtlebot/kobuki/sensors/rplidar", LaserScan, scan_cb, queue_size=1)
+# rospy.Subscriber("/scan", LaserScan, scan_cb, queue_size=1)#! gazebo topic
+rospy.Subscriber("/turtlebot/kobuki/sensors/rplidar", LaserScan, scan_cb, queue_size=1)#! stonefish lidar topic
 rospy.spin()
